68bea7e5b90349db11c78f38b337415055c46973
Eyoba_Smax <eyobsmax@gmail.com>
2025-05-28 20:31:56 +0300

    final touches

--- src/components/main/watched/WatchedSummary.jsx
+++ src/components/main/watched/WatchedSummary.jsx
@@ -2,7 +2,6 @@ import InfoList from "../InfoList";
 const average = (arr) =>
   arr.reduce((acc, cur, index, arr) => acc + cur / arr.length, 0);
 export default function WatchedSummary({ watched }) {
-  console.log(watched);
   const avgImdbRating = average(watched.map((movie) => movie.imbd_rating));
   const avgUserRating = average(watched.map((movie) => movie.user_rating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
