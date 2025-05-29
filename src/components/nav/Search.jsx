ab44296a6a42af3f2638b0738fe3534572d0baeb
Eyoba_Smax <eyobsmax@gmail.com>
2025-05-29 23:31:53 +0300

    adding focus and some ux improvements to the app

--- src/components/nav/Search.jsx
+++ src/components/nav/Search.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useRef } from "react";
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    inputEl.current.focus();
+  });
+
+  useEffect(() => {
+    const callBack = (e) => {
+      if (e.code !== "Enter") return;
+      if (document.activeElement === inputEl.current) return;
+      inputEl.current.focus();
+      setQuery("");
+    };
+
+    document.addEventListener("keydown", callBack);
+
+    return () => document.removeEventListener("keydown", callBack);
+  }, [setQuery]);
+
   return (
     <input
+      ref={inputEl}
       className="search"
       type="text"
       placeholder="Search movies..."
